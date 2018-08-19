840a972061bc77e93b976840b95741a1e2851451
Eddie Harris <harrisejm@gmail.com>
2018-08-18 17:41:04 -0700

    add FEATURED, FOOTER, HEADLINES and NAVIGATION components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FeaturedComponent } from './featured/featured.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { HeadlinesComponent } from './headlines/headlines.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    FeaturedComponent
+    FeaturedComponent,
+    NavigationBarComponent,
+    HeadlinesComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
