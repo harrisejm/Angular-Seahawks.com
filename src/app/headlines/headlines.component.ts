b617a22b8e1701e2a1a5bf37d8ee458b64487d48
Eddie Harris <harrisejm@gmail.com>
2018-08-19 22:53:04 -0700

    add hide headline feature in the featured component. Add styling to footer component

--- src/app/headlines/headlines.component.ts
+++ src/app/headlines/headlines.component.ts
@@ -7,6 +7,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeadlinesComponent implements OnInit {
 
+
+   headlines = !null;
+
+  roundUp = null;
+
+  showVideo = null;
+
+  showHeadLines(show) {
+    this.headlines = show;
+    this.roundUp = null;
+    this.showVideo = null;
+    // document.getElementById("selectHeadlines").style.borderBottom = "solid";
+    // let y = document.getElementById("selectRoundUp");
+    // y.style.borderBottom = "none";
+    // let z = document.getElementById("selectVideos");
+    // y.style.borderBottom = "none";
+    }
+
+  ShowRoundUp(show) {
+    this.roundUp = show;
+    this.headlines = null;
+    this.showVideo = null;
+    }
+
+  ShowVideo(show) {
+    this.showVideo = show;
+    this.headlines = null;
+    this.roundUp = null;
+  }
+
+//   hideShow() {
+//     let x = document.getElementById(this.potionId);
+//     if (x.style.display === "none") {
+//         x.style.display = "block";
+//     } else {
+//         x.style.display = "none";
+//     }
+// }
+
+
+
   constructor() { }
 
   ngOnInit() {
