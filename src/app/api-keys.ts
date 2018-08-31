49fce4e767085a6c8e5952d55825e2f9f8ac383d
Eddie Harris <harrisejm@gmail.com>
2018-08-31 16:37:03 -0700

    remove API key

--- src/app/api-keys.ts
+++
@@ -1,9 +0,0 @@
-export const masterFirebaseConfig = {
-
-    apiKey: "AIzaSyAHeufUSb3SEmXIMKLWHq0Mr1K59iue-5A",
-    authDomain: "comments-68196.firebaseapp.com",
-    databaseURL: "https://comments-68196.firebaseio.com",
-    storageBucket: "comments-68196.appspot.com",
-    messagingSenderId: "775419134504"
-
-  };
